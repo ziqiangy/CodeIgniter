afef2de8f6ecafb1e81f67a5e5bf97bab13b8e06
Peter Yuan <peter.bond40@gmail.com>
2023-07-28 19:47:25 -0500

    Make php xdebug configed and working in vs code, both in current open script and listen for xdebug.

---
+++ run.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * what we did today?
+ * 
+ * Make php xdebug configed and working in vs code, both in current open script and listen for xdebug.
+ * The vscode debug can also work as a script execute for any single php files.
+ * 
+ * Secondly, the we have php 7.4 running in windows cmd, just type 'php run.php', it can run.
+ * 
+ * Or if you want to run interactive mode of php, type 'php -a'.
+ */
+echo "hello";
+// phpinfo();
+// xdebug_info();
+// echo ldks;
+print_r($_SERVER);
+// echo "hello world";
