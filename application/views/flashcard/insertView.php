bb8e8a180d914c3075a79a22fc274086817e3433
Peter Yuan <peter.bond40@gmail.com>
2023-11-10 14:48:07 -0600

    update flashcards views css

--- application/views/flashcard/insertView.php
+++ application/views/flashcard/insertView.php
@@ -1,5 +1,48 @@
 
-    <?php echo form_open('flashcard/insertOne'); ?>
+
+
+
+
+
+
+<style>
+    .box-father{
+        display:flex;
+        justify-content:center;
+    }
+    .box-child{   
+        width:400px;
+        height:300px;
+        border:3px solid black;
+        background-color:#fff7d1;
+        border-radius:10px;
+        padding:15px;
+        
+    }
+
+    .h-title{
+        text-align:center;
+    }
+    .text-father{
+        display:flex;
+        justify-content:center;
+    }
+</style>
+
+
+<div class="box-father">
+    <div class="box-child">
+        <div class="h-title">
+            <h1>Insert a New Flashcard</h1>
+        </div>
+
+
+    
+        <div class="text-father">
+            <div class="text-child">            
+                
+
+            <?php echo form_open('flashcard/insertOne'); ?>
     <label for="term">Term:</label><br>
     <input type="text" name="term" value=""/><br>
     <label for="definition">Definition:</label><br>
@@ -12,5 +55,10 @@
         <?php } ?>
     </select>
     <input type="submit" value="Submit"/>
-
 </form>
+
+
+            </div>
+        </div>
+    </div>
+</div>
--- application/views/flashcard/updateView.php
+++ application/views/flashcard/updateView.php
@@ -1,23 +1,68 @@
-<?php echo form_open('flashcard/updateOne'); ?>
 
-<label for="term">Term:</label><br>
-    <input type="text" name="term" value="<?php echo $user_data["term"] ?>"/><br>
-    <label for="definition">Definition:</label><br>
-    <textarea type="text" name="definition" rows="5" cols="50"><?php echo $user_data["definition"] ?></textarea><br>
-    <select name="category_id">
-        <option value="" 
-        <?php  
-        if(empty($user_data["category_id"])) echo "selected";
-        ?>
-        >No Category</option>
-        <?php foreach($fcs_data as $d){ ?>
-            <option value="<?php echo $d["id"] ?>" <?php if($user_data["category_id"]==$d["id"]) echo"selected";?>><?php echo $d["name"] ?></option>
-        <?php } ?>
-    </select>
-    <input type="hidden" name="id" value="<?php echo $user_data["id"] ?>"/><br>
-    <input type="submit" value="Update"/>
+<style>
+    .box-father{
+        display:flex;
+        justify-content:center;
+    }
+    .box-child{   
+        width:400px;
+        height:300px;
+        border:3px solid black;
+        background-color:#fff7d1;
+        border-radius:10px;
+        padding:15px;
+        
+    }
 
-</form>
-<?php echo anchor("flashcard/disable/".strval($user_data["id"]),"disable"); ?>
-<br>
-<?php echo anchor("flashcard/delete/".strval($user_data["id"]),"delete"); ?>
+    .h-title{
+        text-align:center;
+    }
+    .text-father{
+        display:flex;
+        justify-content:center;
+    }
+</style>
+
+
+<div class="box-father">
+    <div class="box-child">
+        <div class="h-title">
+            <h1>Update a Flashcard</h1>
+        </div>
+    
+        <div class="text-father">
+            <div class="text-child">      
+                
+            <?php echo form_open('flashcard/updateOne'); ?>
+
+                <label for="term">Term:</label><br>
+                    <input type="text" name="term" value="<?php echo $user_data["term"] ?>"/><br>
+                    <label for="definition">Definition:</label><br>
+                    <textarea type="text" name="definition" rows="5" cols="50"><?php echo $user_data["definition"] ?></textarea><br>
+                    <select name="category_id">
+                        <option value="" 
+                        <?php  
+                        if(empty($user_data["category_id"])) echo "selected";
+                        ?>
+                        >No Category</option>
+                        <?php foreach($fcs_data as $d){ ?>
+                            <option value="<?php echo $d["id"] ?>" <?php if($user_data["category_id"]==$d["id"]) echo"selected";?>><?php echo $d["name"] ?></option>
+                        <?php } ?>
+                    </select>
+                    <input type="hidden" name="id" value="<?php echo $user_data["id"] ?>"/><br>
+                    <input type="submit" value="Update"/>
+
+                </form>
+                <?php echo anchor("flashcard/disable/".strval($user_data["id"]),"disable"); ?>
+                <br>
+                <?php echo anchor("flashcard/delete/".strval($user_data["id"]),"delete"); ?>
+
+
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
