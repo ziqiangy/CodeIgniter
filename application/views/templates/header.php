4fd3741ec3e49367bced4f34af0eadbd8ea7a9df
Peter Yuan <peter.bond40@gmail.com>
2023-12-08 15:44:16 -0600

    organize nav bar with dropdown list

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,7 @@
   <title>ZIQIANG YUAN MANAGEMENT APP</title>
   <link rel="icon" type="image/x-icon" href="/pictures/logo-sq.jpg">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="/node_modules/jquery/dist/jquery.min.js"></script>
 	
 </head>
@@ -44,19 +45,104 @@
   background-color: #04AA6D;
   color: white;
 }
+
+
+
+/* drop down */
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.dropbtn {
+  background-color: red;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
 </style>
 
 
 <div class="topnav">
-  <?php echo anchor("flashcard/oneCardView","Flashcard Study"); ?>
+  
+
+  
+
+
+  <div class="dropdown">
+    <button class="dropbtn">Flashcard 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+    <?php echo anchor("flashcard/oneCardView","Flashcard Study"); ?>
   <?php echo anchor("flashcard/displayAllList","List Flashcards"); ?>
   <?php echo anchor("flashcard/insertView","Insert Flashcard"); ?>
-  <?php echo anchor("flashcate/list","Show My Categories"); ?>
+  <?php echo anchor("flashcard/displayMultiInsert","Insert Multi"); ?>
+    </div>
+  </div> 
+
+
+  <div class="dropdown">
+    <button class="dropbtn">Flashcard Categories 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+    <?php echo anchor("flashcate/list","Show My Categories"); ?>
   <?php echo anchor("flashcate/insert","Insert Category"); ?>
   <?php echo anchor("flashcard/searchByCate","Search By Category"); ?>
-  <?php echo anchor("flashcard/displayMultiInsert","Insert Multi"); ?>
-  <?php echo anchor("quicknote/insert","Insert a Note"); ?>
+    </div>
+  </div> 
+
+
+  <div class="dropdown">
+    <button class="dropbtn">Quick Notes 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+    <?php echo anchor("quicknote/insert","Insert a Note"); ?>
   <?php echo anchor("quicknote/list","Show My Notes"); ?>
+    </div>
+  </div> 
+
+
+  
+
+
 
   <?php if(isset($_SESSION["user_id"])){
     echo anchor("user/profile",$_SESSION["username"],array('class' => 'split'));
