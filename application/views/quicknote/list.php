02be779ca4c35ab88fda255b2c1ea6b71c4c5547
Peter Yuan <peter.bond40@gmail.com>
2024-01-03 16:06:33 -0600

    2 rows max for quick notes list

--- application/views/quicknote/list.php
+++ application/views/quicknote/list.php
@@ -20,41 +20,47 @@
         display:flex;
         justify-content:center;
     }
+
+    td:nth-child(1) {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        float: left;	
+        width: 100%;
+        word-break: break-all;
+        text-overflow: ellipsis;
+
+        /* white-space: wrap;
+        overflow: hidden;
+        text-overflow:ellipsis; */
+
+        /* margin-top:10px; */
+        /* padding-bottom:10px; */
+        margin-bottom:25px;
+        /* border-bottom:2px solid black; */
+
+    }
 </style>
 
 
 <div class="box-father">
     <div class="box-child">
         <div class="h-title">
-            <h1>Notes list</h1>
+            <h1>Notes</h1>
         </div>
-
-
-    
         <div class="text-father">
-            <div class="text-child">            
-                
-
-
-
-            <table>
-    <tr>
-        <th>Quick notes list</th>
-        
-    </tr>
-    <?php foreach($data as $d) { ?>
-            <tr class="">
-                <td><?php echo anchor("quicknote/update/".$d['id'],$d['content']) ?></td>
-                
-            </tr>
-        <?php } ?>
-</table>
-
-
-
-            
-
-
+            <div class="text-child">         
+                <table>
+                    <tr>
+                        <th>Notes</th>
+                    </tr>
+                    <?php foreach($data as $d) { ?>
+                            <tr class="">
+                                <td><?php echo anchor("quicknote/update/".$d['id'],$d['content']); ?></td>           
+                            </tr>
+                        <?php } ?>
+                </table>
             </div>
         </div>
     </div>
